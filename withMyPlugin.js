e3b3b51b7af15f730b0dd121e758cd87505210f6
Aman Mittal <amandeepmittal@live.com>
2023-09-01 21:06:25 +0530

    Initial commit

---
+++ withMyPlugin.js
@@ -0,0 +1,24 @@
+const { withAndroidManifest } = require("expo/config-plugins");
+// import { withAndroidManifest } from "expo/config";
+
+const withAndroidQueries = (config) => {
+  return withAndroidManifest(config, (config) => {
+    config.modResults.manifest.queries = [
+      {
+        intent: [
+          {
+            action: [{ $: { "android:name": "android.intent.action.SENDTO" } }],
+            data: [{ $: { "android:scheme": "mailto" } }],
+          },
+          {
+            action: [{ $: { "android:name": "android.intent.action.DIAL" } }],
+          },
+        ],
+      },
+    ];
+
+    return config;
+  });
+};
+
+module.exports = withAndroidQueries;
